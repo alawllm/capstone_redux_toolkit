b0ad0b57a8d9b05c6c361474537ee027be4aa6d1
alawllm <alicja.willam@gmail.com>
2023-07-25 16:50:54 +0200

    switching to redux-toolkit

--- src/store/user/user.reducer.js
+++ src/store/user/user.reducer.js
@@ -1,16 +1,26 @@
-import { USER_ACTION_TYPES } from './user.types';
+import { createSlice } from '@reduxjs/toolkit';
 
 const INITIAL_STATE = {
   currentUser: null,
 };
 
-export const userReducer = (state = INITIAL_STATE, action) => {
-  const { type, payload } = action;
-
-  switch (type) {
-    case USER_ACTION_TYPES.SET_CURRENT_USER:
-      return { ...state, currentUser: payload };
-    default:
-      return state;
+//replaces reducers, actions, types
+export const userSlice = createSlice({
+  name: 'user',
+  initialState: INITIAL_STATE,
+  reducers: {
+    //action - every action that passes through the store
+    //action that we create
+    //creates action creator and reducer update
+    setCurrentUser(state, action) {
+      //looks mutable but state is not mutated
+      //new object created under the hood
+      state.currentUser = action.payload
+    }
   }
-};
+})
+
+//destructuring actions off of the slice
+export const { setCurrentUser } = userSlice.actions;
+//replaces reducer
+export const userReducer = userSlice.reducer;
