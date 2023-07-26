12e790a934f4a0488cbcbb5c5bdce259e3ff28cf
alawllm <alicja.willam@gmail.com>
2023-07-26 15:02:37 +0200

    setting up stripe with serverless

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -14,11 +14,36 @@ const PaymentForm = () => {
         if (!stripe || !elements) {
             return;
         }
+        const response = await fetch('/.netlify/functions/create-payment-intent', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ amount: 100000 })
+        }).then(res => res.json())
 
+        const clientSecret = response.paymentIntent.client_secret;
+
+        const paymentResult = await stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: elements.getElement(CardElement),
+                billing_details: {
+                    name: 'Ala Marchewka'
+                }
+            }
+        })
+
+        if (paymentResult.error) {
+            alert(paymentResult.error)
+        } else {
+            if (paymentResult.paymentIntent.status === 'succeeded') {
+                alert('Payment Succesfull')
+            }
+        }
     }
     return (
         <PaymentFormContainer>
-            <FormContainer>
+            <FormContainer onSubmit={paymentHandler}>
                 <CardElement />
                 <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay now</Button>
             </FormContainer>
